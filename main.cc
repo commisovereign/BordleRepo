a5e86ee3ab7519e46df6a6e3389327d36e487db7
commisovereign <zaidnolley@gmail.com>
2025-04-02 15:13:29 -0500

    started filling out main()

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include "bordle.h"
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::string playerName;
+
+    std::cout << "Enter player name: ";
+    std::cin >> playerName;
+    
+
+    return;
+}
